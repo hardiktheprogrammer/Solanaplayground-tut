30a62e35da656978b62e00e64a8ffb2bddda5143
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-19 03:06:42 +0530

    Init contract

--- VestingContract/files/lib.rs
+++ VestingContract/files/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Transfer};
+
 declare_id!("");
 
 #[program]
@@ -11,4 +13,30 @@ pub mod Vesting_Contract {
         vesting.expiry = expiry;
         Ok(())
     }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        let vesting = &mut ctx.accounts.vesting;
+
+        vesting.expiry = expiry;
+
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+
+pub struct Initialize<'info> {
+    #[account(init, payer = user , space = 8 + 8 ) ]
+    pub Vesting: ProgramAccount<'info, Vesting>,
+
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+
+pub struct Claim<'info> {
+    #[account(mut)]
+    pub vesting: ProgramAccount<'info, Vesting>,
+    pub clock: Sysvar<'info, Clock>,
 }
